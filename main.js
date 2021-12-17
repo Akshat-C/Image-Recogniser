0350cde8c601bb34da83cc6dbbd6b99853400ac4
Akshat-C <85225437+Akshat-C@users.noreply.github.com>
2021-12-17 19:13:44 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+Webcam.set({
+    height: 250,
+    width: 250,
+    image_format: "png",
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
+Webcam.attach("#camera");
+
+function take_image()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = "<img id='captured_img' src="+data_uri+">";
+    });
+}
+
+console.log("ml5 version is:", ml5.version);
+
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/ffs4c1ZIk/model.json", modelLoaded);
+
+function modelLoaded()
+{
+    console.log("model has been loaded");
+}
+
+function identify_image()
+{
+    img = document.getElementById("captured_img");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if (error)
+    {
+        console.error(error);
+    } else
+    {
+        console.log(results);
+        document.getElementById("object_name").innerHTML = results[0].label;
+        percen =  results[0].confidence * 100;
+        document.getElementById("object_accuracy").innerHTML = percen.toFixed(2) + " % ";
+    }
+}
